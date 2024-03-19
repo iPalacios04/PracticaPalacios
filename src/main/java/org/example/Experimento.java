c09e31eddf78d4acd8f9f7780d6633b2e1189b90
Iñigo Palacios <palaciosinigo04@gmail.com>
2024-03-19 17:14:26 +0100

    getters y setters

--- src/main/java/org/example/Experimento.java
+++ src/main/java/org/example/Experimento.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Experimento {
+public class Experimento{
 
     private String nombre;
     private int numBacterias;
@@ -9,11 +9,51 @@ public class Experimento {
     private Dosis dosisComida;
     private Poblacion[] poblaciones;
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public int getNumBacterias() {
+        return numBacterias;
+    }
 
+    public void setNumBacterias(int numBacterias) {
+        this.numBacterias = numBacterias;
+    }
 
+    public Luminosidad getLuz() {
+        return luz;
+    }
 
+    public void setLuz(Luminosidad luz) {
+        this.luz = luz;
+    }
 
+    public double getTemperatura() {
+        return temperatura;
+    }
 
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    public Dosis getDosisComida() {
+        return dosisComida;
+    }
+
+    public void setDosisComida(Dosis dosisComida) {
+        this.dosisComida = dosisComida;
+    }
+
+    public Poblacion[] getPoblaciones() {
+        return poblaciones;
+    }
+
+    public void setPoblaciones(Poblacion[] poblaciones) {
+        this.poblaciones = poblaciones;
+    }
 }
--- src/main/java/org/example/Poblacion.java
+++ src/main/java/org/example/Poblacion.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import java.util.*;
-public class Poblacion {
+public class Poblacion extends Experimento {
 
     private String nombre;
     private Date fechaInicial;
@@ -11,4 +11,67 @@ public class Poblacion {
     private Luminosidad luz;
     private Dosis dosisComida;
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaInicial() {
+        return fechaInicial;
+    }
+
+    public void setFechaInicial(Date fechaInicial) {
+        this.fechaInicial = fechaInicial;
+    }
+
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    @Override
+    public int getNumBacterias() {
+        return numBacterias;
+    }
+
+    @Override
+    public void setNumBacterias(int numBacterias) {
+        this.numBacterias = numBacterias;
+    }
+
+    @Override
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    @Override
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    @Override
+    public Luminosidad getLuz() {
+        return luz;
+    }
+
+    @Override
+    public void setLuz(Luminosidad luz) {
+        this.luz = luz;
+    }
+
+    @Override
+    public Dosis getDosisComida() {
+        return dosisComida;
+    }
+
+    @Override
+    public void setDosisComida(Dosis dosisComida) {
+        this.dosisComida = dosisComida;
+    }
 }
